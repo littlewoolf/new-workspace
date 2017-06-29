8f76926d368222e2111924d2c0e2a0f42d4f3e66
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 16:57:58 +0000

    Refactored application controller to include ower application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
-  
-  before_action :set_source
-  
-  def set_source
-    session[:source] = params[:Q] if params[:Q]
-  end
+  include SetSource
 end
