ba0dd7639d69e0ca4e9c8513638eadfa1bea824e
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 15:17:37 +0000

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
      content_tag(:p, greeting, class: "source-greeting")
     end 
   end
+  
+  def copyright_generator
+    WoolfViewTool::Renderer.copyright 'Davin Woolf', 'All rights reserved'
+  end
 end
