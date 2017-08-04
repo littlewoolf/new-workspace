c144edbf5b21441198eeec2e3f9c86898b667271
Jordan Hudgens <jordan@devcamp.com>
2017-08-04 16:24:38 +0000

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,5 +58,16 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
-  
 end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      alert_generator alert
+    end
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "The Huge Failer of Life", sticky: false)
+  end
